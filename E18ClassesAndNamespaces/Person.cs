fb7b5df54fa26374057c590b2ff8eeeb979b1511
Brian Neal <brianbneal@gmail.com>
2022-07-23 12:03:59 -0500

    Add project files.

---
+++ E18ClassesAndNamespaces/Person.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E18ClassesAndNamespaces;
+
+public enum Options 
+{
+    Rock,
+    Paper,
+    Scissors
+}
+
+//accessibility modifiers - private, internal, public
+public class Person
+{
+    //classes have members; specifically Properties, Fields, and Methods/Constructors
+    public Person()
+    {
+
+    }
+
+    public Person(string firstName, string lastName, int age)
+    {
+        _firstName = firstName;
+        _lastName = lastName;
+        Age = age;
+    }
+
+    private string _firstName;
+    private string _lastName;
+
+    //properties need access modifier, type, name
+    public int Age { get; set; }
+    public bool LikesCilantro
+    {
+        get => LikesCilantro;
+        set => Age = value ? 1 : 0;
+    }
+
+    //readonly property, has a getter but no setter, so can only be set in the constructor
+    public string Name => $"{_firstName} {_lastName}";
+    public Job Job { get; set; }
+    
+    //Fields are private, inaccessible outside of this class
+    private string _ssn;
+
+    ////old version of properties
+    //private string _ssn; //private backing field
+     
+    //public string SSN //property
+    //{
+    //    get { return _ssn; }
+    //    set { _ssn = value; }
+    //}
+
+    //methods - what you call a function when it's a class member
+    //describing behavior
+    public int AddNumbers(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    public void SayHello()
+    {
+        Console.WriteLine("Hello");
+    }
+
+}
+
+
---
+++ E18ClassesAndNamespaces/Program.cs
@@ -0,0 +1,70 @@
+﻿using E18ClassesAndNamespaces;
+using E18ClassesAndNamespaces.Models;
+
+//Classes - blueprint for an object of some type
+string nameOfPerson = "Brian";
+Address address = new Address();
+
+//List<type> collection of same type of thing
+
+//Dictionary<> collection of key value pairs
+/*const dog = {
+ * name: "Fido",
+ * age: 3,
+ * 
+ *} 
+ *dog.name
+ *dog.age
+ *dog[name]
+*/
+Person myPeep = new Person("Brian", "Neal", 41);
+Dictionary<string, Person> peopleDictionary = new Dictionary<string, Person>();
+peopleDictionary["Brian"] = myPeep;
+string rockArt = @"_______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)";
+
+
+
+
+
+//maybe use an enum instead of string for the key?  hmmmm.....
+Dictionary<string, string> graphics = new Dictionary<string, string>()
+{
+    {"rock", rockArt },
+    {"Paper",  "paper ascii art"},
+    {"Scissors",  "scissor ascii art"}
+
+};
+
+List<string> options = new List<string> { "Rock", "Paper", "Scissors" };
+
+string userSelection = "Rock";
+
+string artworkToUse = graphics["Rock"];
+
+Console.WriteLine(artworkToUse);
+
+if (options[0] == "Rock")
+{
+
+}
+
+Person otherPerson = new Person();
+Console.WriteLine(otherPerson.Name);
+
+
+//myPeep.Name = "Brian";
+myPeep.Age = 30;
+int otherVariable = myPeep.Age;
+int sum = myPeep.AddNumbers(3, 7);
+Console.WriteLine(myPeep.Name);
+
+
+
+
+
+Console.ReadLine();
